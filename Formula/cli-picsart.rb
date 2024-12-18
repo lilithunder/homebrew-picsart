20af2054979efdcf13e80d13218bcbfa29542d2d
Lilit Margaryan <150243799+lilithunder@users.noreply.github.com>
2024-12-18 14:23:38 +0400

    Update cli-picsart.rb

--- Formula/cli-picsart.rb
+++ Formula/cli-picsart.rb
@@ -3,7 +3,7 @@ require "language/node"
 
 class CliPicsart < Formula
   desc "CLI tool for interacting with the Picsart API"
-  homepage "https://github.com/PicsA/picsart-creative-apis-cli"
+  homepage "https://github.com/PicsArt/picsart-creative-apis-cli"
   url "https://github.com/PicsArt/picsart-creative-apis-cli/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "8c7d9d7bf74a9505b7044dcad09181a13bf0d6c23267bb2e4ef1097d0821a18f"
   license "MIT"
